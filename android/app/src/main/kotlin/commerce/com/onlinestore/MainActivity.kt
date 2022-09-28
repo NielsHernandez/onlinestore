f78c177975907dca783ad998007d39ee3fe22073
Niels Hernandez <niels.hernandezflores@gmail.com>
2022-09-28 16:03:07 -0600

    app backbone

---
+++ android/app/src/main/kotlin/commerce/com/onlinestore/MainActivity.kt
@@ -0,0 +1,6 @@
+package commerce.com.onlinestore
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/user.dart
@@ -0,0 +1,5 @@
+class User{
+
+  String name ="niels";
+  String lastName = "hernandez";
+}
